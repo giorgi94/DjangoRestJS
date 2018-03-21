655040668392457556dd4db3c8c461a34a4d260d
giorgi94 <giorgik1994@gmail.com>
2018-03-22 00:02:43 +0400

    made img clickable

--- apps/pymedia/static/pymedia/widget.js
+++ apps/pymedia/static/pymedia/widget.js
@@ -1,8 +1,13 @@
 function imgLoaded(event) {
-    event.target.parentElement.style.width = event.target.getBoundingClientRect().width + 'px'
+    event.target.parentElement.style.width =
+        event.target.getBoundingClientRect().width + 'px'
 }
 
 
 function imgClicked(event) {
+    var rect = event.target.getBoundingClientRect()
+    var point = document.querySelector('#'+event.target.id+'-point')
     
+    point.style.left = Math.floor(100*(event.clientX - rect.left)/rect.width) + '%'
+    point.style.top = Math.floor(100*(event.clientY - rect.top)/rect.height) + '%'
 }
