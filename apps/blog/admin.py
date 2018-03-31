8c373d0ef4b42d5e065c02790bb92eea1172404c
giorgi94 <giorgik1994@gmail.com>
2018-03-31 15:55:13 +0400

    some changes

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -2,18 +2,20 @@ from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 from django.contrib import admin
 from .models import Blog, Comment, Category, Tag
-from .forms import BlogForm
+from .forms import BlogAdminForm
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    exclude = ['alias']
+    exclude = ('alias', )
 
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    form = BlogForm
-    exclude = ['alias', ]
+    form = BlogAdminForm
+    exclude = ('alias', )
+
+    # raw_id_fields = ('tags', )
 
 
 @admin.register(Comment)
--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -2,11 +2,15 @@ from django import forms
 from .models import Blog
 
 
-class BlogForm(forms.ModelForm):
+class BlogAdminForm(forms.ModelForm):
 
     class Meta:
         model = Blog
         fields = '__all__'
         widgets = {
             'content': forms.Textarea(attrs={'rich-editor': 'true'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'ui dropdown search'})
         }
+
+    class Media:
+        js = ('tinymce/tinymce.config.js',)
